ba5e781ac6501919e3b2088aeb1dd659f9dbde05
why520crazy <why520crazy@163.com>
2015-02-14 18:17:42 +0800

    启动文件修改

--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,6 @@
 # This is a very simple example
 killall node
 cd /home/ng-nice
-node server.js >> /home/web-log/node-js/ngnice`date +%y%m%d`.log &
+NODE_ENV=production node server.js >> /home/web-log/node-js/ngnice`date +%y%m%d`.log &
 echo "`date +%T`" >> /home/web-log/node-js/ngnice.log
 
