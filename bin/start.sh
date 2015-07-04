ca2efa5199a278aeb80eb789ac2206542c7c3162
why520crazy <why520crazy@163.com>
2015-07-04 15:33:08 +0800

    修改记录日志系统

--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,4 @@
 # This is a very simple example
 killall node
 cd /home/ng-nice
-NODE_ENV=production node app.js >> /home/web-log/node-js/ngnice`date +%y%m%d`.log &
-echo "`date +%T`" >> /home/web-log/node-js/ngnice.log
-
+NODE_ENV=production pm2 start app.js
